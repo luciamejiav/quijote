fc482a53c0f36c845c3e89c03891497b92d88ea8
Lucia Mejia <117280922+luciamejiav@users.noreply.github.com>
2023-10-03 13:01:18 +0200

    Add files via upload

---
+++ 06_quijote/Quijote.cs
@@ -0,0 +1,54 @@
+﻿// crear en la terminal dotnet new console
+using System;
+using Microsoft.VisualBasic;
+
+namespace _06_quijote
+{ //poner el mismo nombre que en el RootNamespace 
+    internal class Quijote
+    {
+        static void Main(string[] args)
+        {
+            string quijote = """
+            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, 
+            no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, 
+            adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca
+            que carnero, salpicón las más noches, duelos y quebrantos los sábados, 
+            lantejas los viernes, algún palomino de añadidura los domingos, 
+            consumían las tres partes de su hacienda. El resto della concluían 
+            sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos 
+            de lo mesmo, y los días de entresemana se honraba con su vellorí de 
+            lo más fino. Tenía en su casa una ama que pasaba de los cuarenta y una 
+            sobrina que no llegaba a los veinte, y un mozo de campo y plaza que así 
+            ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro 
+            hidalgo con los cincuenta años. Era de complexión recia, seco de carnes, 
+            enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que 
+            tenía el sobrenombre de «Quijada», o «Quesada», que en esto hay alguna 
+            diferencia en los autores que deste caso escriben, aunque por conjeturas 
+            verisímiles se deja entender que se llamaba «Quijana». Pero esto 
+            importa poco a nuestro cuento: basta que en la narración dél no se salga un 
+            punto de la verdad.
+            """;
+
+            Console.WriteLine("¿Qué palabra desea buscar?: ");
+            string palabra = Console.ReadLine();
+
+            string[] buscar = quijote.Split(new char[] { ' ', ',', '.', ':' });
+            int contador = 0;
+
+            for (int i = 0; i < buscar.Length; i++)
+            {
+                if (buscar[i].ToLower() == palabra) //pasamos las palabras a minúscula para que no haya errores
+                {
+                    contador++; //contamos las palabras
+                    Console.WriteLine($"La palabra {palabra} está en la posición {i + 1}");
+                }
+            }
+            Console.WriteLine($"La palabra {palabra} se repite {contador} veces");
+
+            if (contador == 0)
+            {
+                Console.WriteLine($"La palabra {palabra} no aparece");
+            }
+        }
+    }
+}
